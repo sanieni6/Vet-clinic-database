17e10f38162420dbbbaf380223d8f48de3890039
Luis Sanchez <luissanchezzamora23@gmail.com>
2023-05-04 14:51:10 -0500

    Update squema.sql: Create new join tables

--- schema.sql
+++ schema.sql
@@ -30,3 +30,34 @@ ALTER TABLE animals ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY;
 ALTER TABLE animals DROP COLUMN species;
 ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(id);
 ALTER TABLE animals ADD COLUMN owner_id INT REFERENCES owners(id);
+
+/* ADD new tables. Activities Day 4 */
+
+CREATE TABLE vets (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  age INTEGER,
+  date_of_graduation DATE
+);
+
+
+CREATE TABLE specializations (
+    id SERIAL PRIMARY KEY,
+    vet_id INT  REFERENCES vets (id),
+    species_id INT  REFERENCES species (id)
+);
+
+
+CREATE TABLE visits (
+    id SERIAL PRIMARY KEY,
+    animal_id INT REFERENCES animals(id),
+    vet_id INT REFERENCES vets(id),
+    visit_date DATE
+);
+
+
+
+
+
+
+
